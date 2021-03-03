0177e980d22e3ca2eecd88b31f6fc217096bf941
Laraib Khaild <laraibkhalid7861@gmail.com>
2021-03-03 23:15:58 +0500

    make converter and implement converter to challenges page

--- FitnessApp/ViewModels/ChallengesViewModel.cs
+++ FitnessApp/ViewModels/ChallengesViewModel.cs
@@ -1,11 +1,30 @@
 ﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FitnessApp.ViewModels
 {
-    public class ChallengesViewModel:BaseViewModel
+    public class ChallengesViewModel : BaseViewModel
     {
+        public ChallengesViewModel()
+        {
+            SelectCategoryCommand = new Command<string>(OnSelectCategory);
+        }
+        int selectedCategoryIndex;
+        public int SelectedCategoryIndex
+        {
+            get => selectedCategoryIndex;
+            set => SetProperty(ref selectedCategoryIndex, value);
+        }
+        public Command<string> SelectCategoryCommand { get; set; }
+        void OnSelectCategory(string index)
+        {
+            if(int.TryParse(index, out int i))
+            {
+                SelectedCategoryIndex = i;
+            }
+        }
     }
 }
